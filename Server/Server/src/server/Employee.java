54a7bb603a3bbdec7b3e0c05d6b7e7c62a17cfc5
fionntmcc <127796057+fionntmcc@users.noreply.github.com>
2024-11-20 14:12:46 +0000

    update employee and report

--- Server/Server/src/server/Employee.java
+++ Server/Server/src/server/Employee.java
@@ -1,10 +1,60 @@
 package server;
 
-public record Employee(String name, 
-		String id,
-		String email,
-		String password,
-		Department dept,
-		Role role) {
+public class Employee {
+	
+	private static int idCounter = 0;
+	private int idNum;
+	private String name, email, password;
+	private Department dept;
+	private Role role;
+	
+	public Employee(String name, 
+			String email,
+			String password,
+			Department dept,
+			Role role) {
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.dept = dept;
+		this.role = role;
+		this.idNum = idCounter++;
+	}
+	
+	public String id() {
+		return ("EMP" + idNum);
+	}
+	
+	public String name() {
+		return name;
+	}
+
+	public String email() {
+		return email;
+	}
+
+	public String password() {
+		return password;
+	}
+
+	public Department dept() {
+		return dept;
+	}
+
+	public Role role() {
+		return role;
+	}
+	
+	@Override
+	public String toString() {
+		return ("Employee id: " + id() + "\n" +
+				"Name: " + name() + "\n" +
+				"Email: " + email() + "\n" +
+				"Dept: " + dept() + "\n" +
+				"Role: " + id() + "\n" +
+				"Password: " + password() + "\n"
+				);
+	}
+	
 
 }
--- Server/Server/src/server/Report.java
+++ Server/Server/src/server/Report.java
@@ -2,10 +2,67 @@ package server;
 
 import java.time.LocalDate;
 
-public record Report(String id, 
-		ReportType type, 
-		LocalDate date, 
-		String empId, 
-		ReportStatus status) {
+public class Report {
+	
+	private ReportType type;
+	private LocalDate date;
+	private String empId;
+	private ReportStatus status;
+	
+	
+	private static int idCounter = 0;
+	private int idNum;
+	
+	public Report( 
+			ReportType type, 
+			LocalDate date, 
+			String empId, 
+			ReportStatus status) {
+		this.type = type;
+		this.date = date;
+		this.empId = empId;
+		this.status = status;
+		
+		this.idNum = idCounter++;
+	}
+	
+	public ReportType type() {
+		return type;
+	}
+
+
+
+	public LocalDate date() {
+		return date;
+	}
+
+
+
+	public String empId() {
+		return empId;
+	}
+
+
+
+	public ReportStatus status() {
+		return status;
+	}
+
+
+
+	public String id() {
+		return ("R" + idNum);
+	}
+
+	@Override
+	public String toString() {
+		return ("Report id: " + id() + "\n" +
+				"Type: " + type() + "\n" +
+				"Date: " + date().toString() + "\n" +
+				"Assigned Employee: " + empId() + "\n" +
+				"Status: " + status() + "\n"
+				);
+	}
+	
 
 }
